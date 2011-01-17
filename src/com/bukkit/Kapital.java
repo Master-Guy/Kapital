daa93024a6e8869f80db73c56b8cb11009cd1d6f
Antony Derham <antonyderham@gmail.com>
2011-01-17 18:17:30 +0000

    Changed logging methods, and added name and version strings

--- src/com/bukkit/Kapital.java
+++ src/com/bukkit/Kapital.java
@@ -2,6 +2,7 @@ package com.bukkit;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -17,21 +18,26 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 
 public class Kapital extends JavaPlugin {
+	public static final Logger log = Logger.getLogger("Minecraft");
+	
     private final KapitalPlayerListener playerListener = new KapitalPlayerListener(this);
     private final KapitalBlockListener blockListener = new KapitalBlockListener(this);
     private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+    
+    public static String name = "Kapital";
+    public static String version = "0.0.1";
 
     public Kapital(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
         super(pluginLoader, instance, desc, folder, plugin, cLoader);
     }
 
     public void onEnable() {
+    	log.info(name + " v" + version + " loaded");
         PluginManager pm = getServer().getPluginManager();
-        log("Kapital loaded!");
     }
     
     public void onDisable() {
-        System.out.println("Goodbye world!");
+    	log.info(name + " v" + version + " disabled");
     }
     
     public boolean isDebugging(final Player player) {
@@ -44,9 +50,5 @@ public class Kapital extends JavaPlugin {
 
     public void setDebugging(final Player player, final boolean value) {
         debugees.put(player, value);
-    }
-    
-    public void log(String logText) {
-    	System.out.println(logText);
     }
 }

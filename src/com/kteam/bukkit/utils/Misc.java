28516a353b755817da5b2556692453831a5ee2cd
Antony Derham <antonyderham@gmail.com>
2011-01-20 22:36:36 +0000

    Ridded Misc of StringBuffer

--- src/com/kteam/bukkit/utils/Misc.java
+++ src/com/kteam/bukkit/utils/Misc.java
@@ -2,14 +2,13 @@ package com.kteam.bukkit.utils;
 
 public class Misc {
 	public static String arrayToString(String[] a, String separator) {
-	    StringBuffer result = new StringBuffer();
+	    String result = new String();
 	    if (a.length > 0) {
-	        result.append(a[0]);
+	        result = a[0];
 	        for (int i=1; i<a.length; i++) {
-	            result.append(separator);
-	            result.append(a[i]);
+	            result = result + separator + a[i];
 	        }
 	    }
-	    return result.toString();
+	    return result;
 	}
 }
